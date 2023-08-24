140772f6889315be7ae8841bb272c110e565ab71
Lotte Eliasson <lottefacereliasson@hotmail.com>
2023-08-24 11:39:29 +0200

    u database con

--- src/test/java/demo/demoto/DemoToApplicationTests.java
+++ src/test/java/demo/demoto/DemoToApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoToApplicationTests {
-    //Tester om hele Spring boot miljøet virker, Hvis køres fra Maven får den ikke Environment Variable med, da sat i IntelliJ.
+
     //@Test
     void contextLoads() {
     }
